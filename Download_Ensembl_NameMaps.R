d8644195a2760fd3856e95892346009c2b840097
tallulandrews <tallulandrews@gmail.com>
2020-10-04 22:50:22 -0400

    Add Ensembl Stuff

---
+++ Download_Ensembl_NameMaps.R
@@ -0,0 +1,31 @@
+require("biomaRt")
+ensembl = useMart("ensembl", mirror='useast')
+ensg = useDataset("hsapiens_gene_ensembl", ensembl)
+musg = useDataset("mmusculus_gene_ensembl", ensembl)
+rat = useDataset("rnorvegicus_gene_ensembl", ensembl)
+
+attributes = listAttributes(musg)
+
+
+ensg2musg <- getBM(attributes=c('ensembl_gene_id', "mmusculus_homolog_ensembl_gene"), mart = ensg)
+ensg2musg <- ensg2musg[ensg2musg[,2] != "",]
+
+ensg2rat <- getBM(attributes=c('ensembl_gene_id', "rnorvegicus_homolog_ensembl_gene"), mart = ensg)
+ensg2rat <- ensg2rat[ensg2rat[,2] != "",]
+
+musg2rat <- getBM(attributes=c('ensembl_gene_id', "rnorvegicus_homolog_ensembl_gene"), mart = musg)
+musg2rat <- musg2rat[musg2rat[,2] != "",]
+
+ensg2symbol <- getBM(attributes=c('ensembl_gene_id', "hgnc_symbol"), mart = ensg)
+ensg2symbol <- ensg2symbol[ensg2symbol[,2] != "",]
+
+
+musg2symbol <- getBM(attributes=c('ensembl_gene_id', "mgi_symbol"), mart = musg)
+musg2symbol <- musg2symbol[musg2symbol[,2] != "",]
+
+rat2symbol <- getBM(attributes=c('ensembl_gene_id', "rgd_symbol"), mart = rat)
+rat2symbol <- rat2symbol[rat2symbol[,2] != "",]
+
+
+
+saveRDS(list(ensg2musg=ensg2musg, ensg2symbol=ensg2symbol, musg2symbol=musg2symbol, ensg2rat=ensg2rat, rat2symbol=rat2symbol, musg2rat=musg2rat), "my_ensembl_name_maps.rds")
---
+++ Ensembl_Stuff.R
@@ -0,0 +1,142 @@
+ensg_map_obj <- readRDS("/cluster/projects/macparland/TA/ExternalData/Ensembl/my_ensembl_name_maps.rds")
+ensg_name_map <- ensg_map_obj[["ensg2symbol"]]
+ensg2musg <- ensg_map_obj[["ensg2musg"]]
+ensg2rat <- ensg_map_obj[["ensg2rat"]]
+musg2rat <- ensg_map_obj[["musg2rat"]]
+musg_name_map <- ensg_map_obj[["musg2symbol"]]
+rat_name_map <- ensg_map_obj[["rat2symbol"]]
+
+h_dups = unique(ensg2musg[(duplicated(ensg2musg[,1])),1])
+m_dups = unique(ensg2musg[(duplicated(ensg2musg[,2])),2])
+ensg2musg_one2one <- ensg2musg[!(ensg2musg[,1] %in% h_dups) & !(ensg2musg[,2] %in% m_dups),]
+
+map_symbol_ensg <- function(genes, is.org=c("Hsap","Mmus","Rat"), is.name=c("symbol","ensg")) {
+	if (is.org[1] == "Hsap") {
+		if(is.name[1]=="symbol") {
+			new = as.character(ensg_name_map[match(genes, ensg_name_map[,2]),1])
+		} else if (is.name[1] =="ensg") {
+			new = as.character(ensg_name_map[match(genes, ensg_name_map[,1]),2])
+		} else {
+			stop("Unrecognized name type")
+		}
+	} else if (is.org[1] == "Mmus") {
+		if(is.name[1]=="symbol") {
+			new = as.character(musg_name_map[match(genes, musg_name_map[,2]),1])
+		} else if (is.name[1] =="ensg") {
+			new = as.character(musg_name_map[match(genes, musg_name_map[,1]),2])
+		} else {
+			stop("Unrecognized name type")
+		}
+	} else if (is.org[1] == "Rat") {
+		if(is.name[1]=="symbol") {
+			new = as.character(rat_name_map[match(genes, rat_name_map[,2]),1])
+		} else if (is.name[1] =="ensg") {
+			new = as.character(rat_name_map[match(genes, rat_name_map[,1]),2])
+		} else {
+			stop("Unrecognized name type")
+		}
+	} else {
+		stop("Unrecognized organism");
+	}
+#	new[is.na(new)] = as.character(genes[is.na(new)])
+	new[is.na(new)] = ""
+	return(new);
+}
+
+map_Hsap_Mmus <- function(genes, is.org=c("Hsap","Mmus"), one2one=FALSE) {
+	if (one2one) {
+		local_map <- ensg2musg_one2one
+	} else {
+		local_map <- ensg2musg
+	}
+	if (is.org[1] == "Hsap") {
+		new = as.character(local_map[match(genes, local_map[,1]),2])
+	} else if (is.org[1] == "Mmus") {
+		new = as.character(local_map[match(genes, local_map[,2]),1])
+	} else {
+		stop("Unrecognized organism");
+	}
+#	new[is.na(new)] = as.character(genes[is.na(new)])
+	new[is.na(new)] = ""
+	return(new);
+}
+
+map_ensg_across_org <- function(genes, is.org=c("Hsap", "Mmus", "Rat"), out.org=c("Hsap", "Mmus", "Rat"), one2one=FALSE) {
+	if (is.org == "Hsap") {
+		if (out.org == "Mmus") {
+			cross_spp_map <- ensg2musg
+		} else if (out.org == "Rat") {
+			cross_spp_map <- ensg2rat
+		} else {
+			stop("Unrecognized organism")
+		}
+	} else if (is.org == "Mmus") {
+		if (out.org == "Hsap") {
+			cross_spp_map <- cbind(as.character(ensg2musg[,2]), as.character(ensg2musg[,1]))
+		} else if (out.org == "Rat") {
+			cross_spp_map <- musg2rat;
+		} else {
+                        stop("Unrecognized organism")
+                }
+	} else if (is.org == "Rat") {
+		 if (out.org == "Mmus") {
+                        cross_spp_map <- cbind(as.character(musg2rat[,2]), as.character(musg2rat[,1]))
+                } else if (out.org == "Hsap") {
+                        cross_spp_map <- cbind(as.character(ensg2rat[,2]), as.character(ensg2rat[,1]))
+                } else {
+                        stop("Unrecognized organism")
+                }
+	} else {
+		stop("Unrecognized organism")
+	}
+	if (one2one) {
+		first_dup <- unique(cross_spp_map[duplicated(cross_spp_map[,1]),1])
+		second_dup <- unique(cross_spp_map[duplicated(cross_spp_map[,2]),2])
+		cross_spp_map <- cross_spp_map[!(cross_spp_map[,1] %in% first_dup) &
+						!(cross_spp_map[,2] %in% second_dup), ]
+	}
+
+	new = as.character(cross_spp_map[match(genes, cross_spp_map[,1]),2])
+	new[is.na(new)] = ""
+	return(new);
+}
+
+General_Map <- function(genes, in.org=c("Hsap","Mmus","Rat"), in.name=c("symbol","ensg"), out.org=c("Hsap","Mmus", "Rat"), out.name=c("symbol","ensg")) {
+	if (in.org == out.org & in.name == out.name) {
+		# No change
+		return(genes)
+	}
+	if (in.org == out.org) {
+		# change names not spp
+		return(map_symbol_ensg(genes, is.org=in.org, is.name=in.name))
+
+	} else {
+		if (in.name == "symbol") {
+			tmp <- map_symbol_ensg(genes, is.org=in.org, is.name=in.name)
+		} else {
+			tmp <- genes
+		}
+		tmp <- map_ensg_across_org(tmp, is.org=in.org, out.org=out.org)
+		if (out.name =="symbol") {
+			out <- map_symbol_ensg(tmp, is.org=out.org, is.name="ensg")
+		} else {
+			out <- tmp
+		}
+		return(out)	
+	}
+}
+
+M_symbol_2_H_symbol <- function(genes) {
+        tmp <- map_symbol_ensg(genes, is.org="Mmus", is.name="symbol")
+        tmp <- map_Hsap_Mmus(tmp, is.org="Mmus")
+        tmp <- map_symbol_ensg(tmp, is.org="Hsap", is.name="ensg")
+        return(tmp)
+}
+
+H_symbol_2_M_symbol <- function(genes) {
+        tmp <- map_symbol_ensg(genes, is.org="Hsap", is.name="symbol")
+        tmp <- map_Hsap_Mmus(tmp, is.org="Hsap")
+        tmp <- map_symbol_ensg(tmp, is.org="Mmus", is.name="ensg")
+        return(tmp)
+}
+
